226ecca52b88eb5d0486cdc4dda17027984dbde4
ghkd1418 <1004wipi@gmail.com>
2025-04-26 19:21:11 +0900

    chore: model 위치 변경

--- src/main/kotlin/com/example/demo/database/model/Note.kt
+++ src/main/kotlin/com/example/demo/database/model/Note.kt
@@ -1,9 +1,9 @@
-package com.example.demo.model
+package com.example.demo.database.model
 
-import java.time.Instant
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
 
 @Document("notes")
 data class Note(
