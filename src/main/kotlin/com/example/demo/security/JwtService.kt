89629227041f562ced3806077efe848e78a1329c
ghkd1418 <1004wipi@gmail.com>
2025-05-03 12:32:36 +0900

    feat: jwt service 추가

---
+++ src/main/kotlin/com/example/demo/security/JwtService.kt
@@ -0,0 +1,78 @@
+package com.example.demo.security
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.Base64
+import java.util.Date
+
+@Service
+class JwtService(
+    @Value("JWT_SECRET_BASE64") private val jwtSecret: String
+) {
+    private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
+    private val accessTokenValidMs = 15L * 60L *1000L
+    val refreshTokenValidityMs = 30L * 24 * 60 * 60 * 1000L
+
+    private fun generateToken(
+        userId: String,
+        type: String,
+        expiry: Long,
+    ): String {
+        val now = Date()
+        val expireDate = Date(now.time + expiry)
+
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", type)
+            .issuedAt(now)
+            .expiration(expireDate)
+            .signWith(secretKey, Jwts.SIG.HS256)
+            .compact()
+    }
+
+    fun generateAccessToken(userId:String):String {
+        return generateToken(userId, "access", accessTokenValidMs)
+    }
+
+    fun generateRefreshToken(userId:String):String {
+        return generateToken(userId, "refresh", refreshTokenValidityMs)
+    }
+
+    fun validateAccessToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims.get("type", String::class.java) ?: return false
+
+        return tokenType == "access"
+    }
+
+    fun validateRefreshToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims.get("type", String::class.java) ?: return false
+
+        return tokenType == "refresh"
+    }
+
+    fun getUserIdFromToken(token: String):String {
+        val rawToken = if(token.startsWith("bearer")) {
+            token.removePrefix("Bearer ")
+        } else token
+        val claims = parseAllClaims(rawToken) ?: throw IllegalArgumentException("Invalid token.")
+
+        return claims.subject
+    }
+
+    private fun parseAllClaims(token: String): Claims? {
+        return try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
